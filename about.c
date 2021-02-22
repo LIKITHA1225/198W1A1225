5a37e02ded05eab366600aeb61382ee6b34a64b4
Kadiyala Likitha Chowdary <kadiyalalikithachowdhury@gmail.com>
2021-02-22 11:29:52 +0530

    modified using commands

--- about.c
+++ about.c
@@ -2,9 +2,12 @@
 int main()
 {
   int a,b,c;
+   int d;
    printf("enter te values:");
    scanf("%d %d",&a,&b);
    c=a+b;
+   d=a-b;
+   printf("difference is %d",d);
    printf("sum is %d",c);
    return 0;
 }
